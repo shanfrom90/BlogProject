e003965a7f1313737def193c097cd2bf57c7ffcf
shanfrom90 <shanfrom90@gmail.com>
2021-03-06 13:44:27 -0500

    First commit

--- BlogPlatform/Models/Content.cs
+++ BlogPlatform/Models/Content.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 namespace blog_template_practice.Models
 {
     public class Content
-    {
-
+    {   
+        public int Id { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+        public virtual Category Category { get; set; }
+        //may need to add Display Annotation-select Category
+        public int CategoryId { get; set; }
     }
 }
