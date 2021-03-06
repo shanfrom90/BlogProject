899829732eb6054aa26998c69354db201ee87b51
shanfrom90 <shanfrom90@gmail.com>
2021-03-06 14:03:28 -0500

    Category and Content tests passing

--- BlogPlatform/Models/Category.cs
+++ BlogPlatform/Models/Category.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 namespace blog_template_practice.Models
 {
     public class Category
-    {
-
+    { public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual Content Contents { get; set; }
+        public Category (int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
--- BlogPlatform/Models/Content.cs
+++ BlogPlatform/Models/Content.cs
@@ -7,6 +7,23 @@ namespace blog_template_practice.Models
 {
     public class Content
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string PublishDate { get; set; }
+        public string Author { get; set; }
+        public virtual Category Category { get; set; }
+        //may need to add Display Annotation-select Category
+        public int CategoryId { get; set; }
+
+        public Content(int id, string title, string body, string author, string publishDate)
+        {
+            Id = id;
+            Title = title;
+            Body = body;
+            Author = author;
+            PublishDate = publishDate;
+        }
 
     }
 }
